2511ba0a90e67e93c58390945cdcbfc38ca4badd
StackUpZero <boughmichael20@outlook.com>
2026-05-18 13:24:51 +0100

    Add visible menu options

--- main.py
+++ main.py
@@ -5,15 +5,26 @@ day = 1
 running = True
 
 while running:
+    print("1. Check stats\n2. Do small job\n3. Quit")
     choice = input("Choose: ")
     
     if choice == "1":
         print("Cash:", cash)
         print("Reputation:", reputation)
         print("Day:", day)
+        print()
 
-    elif choice == "2":
+    elif choice =="2":
+        print("You did a small Job!")
+        cash += 25
+        reputation += 1
+        day += 1
+        print()
+        
+    elif choice == "3":
         print("Quit")
         running = False
+        
     else:
         print("Invalid choice")
+        print()

b8d7a1232ebf4239f2f33713c92a35ecacab4453
StackUpZero <boughmichael20@outlook.com>
2026-05-18 13:12:09 +0100

    Add player stats menu option

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+cash = 100
+reputation = 0
+day = 1
+
 running = True
 
 while running:
     choice = input("Choose: ")
+    
+    if choice == "i":
+        print(cash)
+        print(reputation)
+        print(day)
     
     if choice == "quit":
         running = False
